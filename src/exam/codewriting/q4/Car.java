aaafc88e2b0bbfb79b6e9cd8499a1765c37bf0a2
charlottegreen29 <c.j.green@wustl.edu>
2025-12-03 21:54:31 -0600

    one left!

--- src/exam/codewriting/q4/Car.java
+++ src/exam/codewriting/q4/Car.java
@@ -12,28 +12,54 @@ package exam.codewriting.q4;
 public class Car {
 
 	// Declare your instance variables here:
+	private double odometer;
+	private double fuelEfficiency;
+	private double fuelTank;
+	private double amountFuel;
+
 	
 	// Write the constructor for the Car class:
 	// (For full credit the constructor should use only two parameters)
+	public Car(double fuelEfficiency, double fuelTank){
+		this.odometer = 0;
+		this.fuelEfficiency = fuelEfficiency;
+		this.fuelTank = fuelTank;
+		this.amountFuel = 0;
+	}
 	
 	/**
 	* Drives the car at the given speed for the given amount of time or until out of fuel.
 	* (Hint: Carefully consider the impact of driving to all instance variables).
 	*/
 	public void drive ( double milesPerHour , double hours ) {
-		
+		double miles = milesPerHour*hours;
+		double galsFuelConsumed = miles/this.fuelEfficiency;
+		if (galsFuelConsumed<=this.amountFuel){
+			this.odometer+=miles;
+			this.amountFuel-=galsFuelConsumed;
+		}
+		else{
+			miles = this.fuelEfficiency*this.amountFuel;
+			this.odometer+=miles;
+			this.amountFuel=0;
+		}
 	}
 	
 	public double getMilesDriven () {
-		return 0; //FIXME
+		return this.odometer;
 	}
 	
 	/**
-	* Adds fuel to the tank as long as it does not exceed the tank’s capacity.
-	* @return portion of fuel that couldn’t be added
+	* Adds fuel to the tank as long as it does not exceed the tankï¿½s capacity.
+	* @return portion of fuel that couldnï¿½t be added
 	*/
 	public double addFuel ( double gallons ) {
-		return 0; //FIXME
+		if (gallons<this.fuelTank){
+			this.amountFuel+=gallons;
+			return 0;
+		}
+		this.amountFuel = fuelTank;
+		return gallons-this.fuelTank;
 	}
 	
 	
